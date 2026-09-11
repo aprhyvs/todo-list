184f1e66c9d08d8d0e0b372099a80aca56681e63
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2026-09-11 14:57:37 +0800

    add initial change project feature
    user can now change project through the select element.

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,28 @@
-import { addTodo, projectList } from "./projectModule.js";
+import { projectList } from "./projectModule.js";
 
 let currentProject;
-// const currentProjectEl = document.getElementById("project");
+
+const currentProjectEl = document.getElementById("project");
+const projectListEl = document.getElementById("project-list");
 
 function setCurrentProject(projectIndex) {
     currentProject = projectList[projectIndex];
+    currentProjectEl.textContent = currentProject.name;
 }
 
+/**
+ * append projectList from ProjectModule.js 
+ * option elements with names and id
+ */
+projectList.forEach((e) => {
+    const projectListOption = document.createElement("option")
+    projectListOption.textContent = e.name
+
+    projectListEl.appendChild(projectListOption)
+});
+
+projectListEl.addEventListener("change", () => {
+    setCurrentProject(projectListEl.selectedIndex);
+});
+
 setCurrentProject(0);
-
-addTodo(currentProject, "test", "desc", "tomorrow", true);
-console.log(currentProject);
-
-// currentProjectEl.textContent = currentProject.name;
