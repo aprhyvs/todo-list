4d01c03f2f1e704a6fe4f6eabf1b95854714a7bf
aprhyvs <173691141+aprhyvs@users.noreply.github.com>
2026-09-20 20:21:34 +0800

    add initial renderTodoItems()

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import { projectList } from "./projectModule.js";
+import "../css/style.css";
+import { projectList, addTodo } from "./projectModule.js";
 
 let currentProject;
 
@@ -26,3 +27,38 @@ projectListEl.addEventListener("change", () => {
 });
 
 setCurrentProject(0);
+
+addTodo(currentProject, "test", "desc", "today", true)
+addTodo(currentProject, "test2", "desc2", "today2", false)
+addTodo(currentProject, "test3", "desc3", "today3", true)
+
+const todoWrapperEl = document.getElementById("todo-list-wrapper");
+
+// code in appending todo items into todoWrapperEl
+const todoTemplate = document.getElementById("todo-item");
+const fragment = document.createDocumentFragment();
+
+/** 
+ * render todoItem class from its currentProject
+ * and append it on todoWrapperEl
+ * @param {*} currentProjectParam 
+ */
+function renderTodoItems(currentProjectParam) {
+    const todoList = currentProjectParam.todoList
+
+    for (const todo of todoList) {
+        console.log(todo)
+        const clone = todoTemplate.content.cloneNode(true)
+
+        clone.querySelector(".todo__title").textContent = todo.title
+        clone.querySelector(".todo__desc").textContent = todo.description
+        clone.querySelector(".todo__due").textContent = todo.dueDate
+        clone.querySelector(".todo__priority").textContent = todo.priority
+
+        fragment.appendChild(clone)
+    }
+
+    todoWrapperEl.appendChild(fragment)
+}
+
+renderTodoItems(currentProject)
